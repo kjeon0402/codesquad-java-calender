fed70d23f7e330e49dbd4e04ff606cf82e4dc9fa
k4709 <k4709@DESKTOP-1M3GHJF>
2021-01-13 16:43:48 +0900

    Calendar_Shedule_Class(Register&Search)

--- src/MyCalendar/Prompt.java
+++ src/MyCalendar/Prompt.java
@@ -99,30 +99,27 @@ public class Prompt {
 		System.out.println("[일정 검색]");
 		System.out.println("날짜를 입력해주세요(yyyy-MM-dd)");
 		String date = s.next();
-		String plan = "";
-		try {
-			plan = c.serchPlan(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			System.err.println("일정 검색 중 오류가 발생했습니다.");
+		PlanItem plan;
+		plan = c.serchPlan(date);
+		if(plan != null) {
+			System.out.println(plan.detail);
+		} else {
+			System.out.println("일정이 없습니다.");
 		}
-		System.out.println(plan);
 	}
-
+	
 	private void cmdRegister(Scanner s, Calendar c) throws ParseException {
 		System.out.println("[새 일정 등록]");
 		System.out.println("날짜를 입력해주세요(yyyy-MM-dd)");
 		String date = s.next();
 		String text = "";
 		System.out.println("일정을 입력해주세요.(문장 끝에 ;을 입력해주세요.)");
-		while (true) {
-			String word = s.next();
+		String word;
+		while (!(word = s.next()).endsWith(";")) {
 			text += word + " ";
-			if(word.endsWith(";")) {
-				break;
-			}
 		}
-		
+		word = word.replace(";", "");
+		text += word;
 		c.registerPlan(date, text);
 	}
 
